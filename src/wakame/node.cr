d280db2675ba877ec2bdd9619542897b7a186a0b
soya_daizu <soya_daizu@protonmail.com>
2022-07-17 16:27:54 +0900

    Fix ordering of MeCabNode

--- src/wakame/node.cr
+++ src/wakame/node.cr
@@ -1,4 +1,38 @@
 module Wakame
+  # Wrapper for the `Wakame::Lib::MeCabNodeT` structure holding the parsed node.
+  #
+  # Access to any missing attributes will be forwarded to the underlying `Wakame::Lib::MeCabNodeT`.
+  # For example, if you want to get the `id` of the node, you can do so by simply trying to access
+  # its member as you normally do with typical objects:
+  # ```
+  # node = MeCabNode.new(node_ptr)
+  # node.id # => 91
+  # ```
+  # For the available attributes, see the documentation of `Wakame::Lib::MeCabNodeT`.
+  struct MeCabNode
+    macro define_stat_method(*names)
+      {% for name in names %}
+        def is_{{name.id}}?
+          stat == LibMeCab::{{name.id.capitalize}}Node
+        end
+      {% end %}
+    end
+
+    forward_missing_to @pointer.value
+    define_stat_method nor, unk, box, eos, eon
+    getter surface, feature
+
+    def is_best?
+      isbest == 1
+    end
+
+    def initialize(@pointer : Lib::MeCabNodeT*)
+      value = @pointer.value
+      @surface = String.new(Slice.new(value.surface, value.length))
+      @feature = String.new(value.feature)
+    end
+  end
+
   module Lib
     # Underlying structure for `Wakame::MeCabNode`. Represents `struct mecab_node_t` in C.
     #
@@ -65,40 +99,6 @@ module Wakame
                      @lc_attr, @posid, @char_type, @stat, @isbest, @alpha,
                      @beta, @prob, @wcost, @cost)
       end
-    end
-  end
-
-  # Wrapper for the `Wakame::Lib::MeCabNodeT` structure holding the parsed node.
-  #
-  # Access to any missing attributes will be forwarded to the underlying `Wakame::Lib::MeCabNodeT`.
-  # For example, if you want to get the `id` of the node, you can do so by simply trying to access
-  # its member as you normally do with typical objects:
-  # ```
-  # node = MeCabNode.new(node_ptr)
-  # node.id # => 91
-  # ```
-  # For the available attributes, see the documentation of `Wakame::Lib::MeCabNodeT`.
-  struct MeCabNode
-    macro define_stat_method(*names)
-      {% for name in names %}
-        def is_{{name.id}}?
-          stat == LibMeCab::{{name.id.capitalize}}Node
-        end
-      {% end %}
-    end
-
-    forward_missing_to @pointer.value
-    define_stat_method nor, unk, box, eos, eon
-    getter surface, feature
-
-    def is_best?
-      isbest == 1
-    end
-
-    def initialize(@pointer : Lib::MeCabNodeT*)
-      value = @pointer.value
-      @surface = String.new(Slice.new(value.surface, value.length))
-      @feature = String.new(value.feature)
     end
   end
 end
