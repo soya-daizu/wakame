b38bc9f09af2ee060af34f7f891bc14814e55595
soya_daizu <soya_daizu@protonmail.com>
2022-07-20 08:59:36 +0900

    Add wrapping struct for Lib::MeCabPathT

---
+++ src/wakame/path.cr
@@ -0,0 +1,26 @@
+require "./lib/*"
+require "./wrapping_struct"
+
+module Wakame
+  struct MeCabPath
+    include WrappingStruct
+
+    # Pointer to the underlying structure.
+    getter pointer
+    resolve_pointers(
+      rnode, lnode,
+      of: Lib::MeCabNodeT, as: MeCabNode
+    )
+    resolve_pointers(
+      rnext, lnext,
+      of: Lib::MeCabPathT, as: MeCabPath
+    )
+    delegate_getters(
+      cost, prob,
+      to: Lib::MeCabPathT
+    )
+
+    def initialize(@pointer : Lib::MeCabPathT*)
+    end
+  end
+end
