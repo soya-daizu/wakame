7ea87920349348378a39a1a26cbd6f6edeb3258d
soya_daizu <soya_daizu@protonmail.com>
2022-07-20 06:40:51 +0900

    Normalize lib doc comments

--- src/wakame/lib/struct.cr
+++ src/wakame/lib/struct.cr
@@ -2,23 +2,22 @@ module Wakame
   module Lib
     @[Extern]
     struct DictionaryInfoT
-      # filename of dictionary
-      # On Windows, filename is stored in UTF-8 encoding
+      # Filename of the dictionary.
       getter filename : LibC::Char*
-      # character set of the dictionary. e.g., "SHIFT-JIS", "UTF-8"
+      # Character set of the dictionary.
       getter charset : LibC::Char*
-      # How many words are registered in this dictionary.
+      # Number of words registered in the dictionary.
       getter size : UInt32
-      # dictionary type
-      # this value should be MECAB_USR_DIC, MECAB_SYS_DIC, or MECAB_UNK_DIC.
+      # Dictionary type
+      # This can be either MECAB_USR_DIC, MECAB_SYS_DIC, or MECAB_UNK_DIC.
       getter type : Int32
-      # left attributes size
+      # Left attributes size
       getter lsize : UInt32
-      # right attributes size
+      # Right attributes size
       getter rsize : UInt32
-      # version of this dictionary
+      # Version of this dictionary.
       getter version : UInt16
-      # pointer to the next dictionary info.
+      # Pointer to the next dictionary info.
       getter next : Wakame::Lib::DictionaryInfoT*
 
       # :nodoc:
@@ -29,17 +28,17 @@ module Wakame
 
     @[Extern]
     struct MeCabPathT
-      # pointer to the right node
+      # Pointer to the right node.
       getter rnode : Wakame::Lib::MeCabNodeT*
-      # pointer to the next right path
+      # Pointer to the next right path.
       getter rnext : Wakame::Lib::MeCabPathT*
-      # pointer to the left node
+      # Pointer to the left node.
       getter lnode : Wakame::Lib::MeCabNodeT*
-      # pointer to the next left path
+      # Pointer to the next left path.
       getter lnext : Wakame::Lib::MeCabPathT*
-      # local cost
+      # Local cost
       getter cost : Int32
-      # marginal probability
+      # Marginal probability
       getter prob : Float32
 
       # :nodoc:
@@ -53,57 +52,58 @@ module Wakame
     # retrieving this struct from its pointer. DO NOT use this struct in your actual code!
     @[Extern]
     struct MeCabNodeT
-      # pointer to the previous node.
+      # Pointer to the previous node.
       getter prev : Wakame::Lib::MeCabNodeT*
-      # pointer to the next node.
+      # Pointer to the next node.
       getter next : Wakame::Lib::MeCabNodeT*
-      # pointer to the node which ends at the same position.
+      # Pointer to the node which ends at the same position.
       getter enext : Wakame::Lib::MeCabNodeT*
-      # pointer to the node which starts at the same position.
+      # Pointer to the node which starts at the same position.
       getter bnext : Wakame::Lib::MeCabNodeT*
-      # pointer to the right path.
-      # this value is NULL if MECAB_ONE_BEST mode.
+      # Pointer to the right path.
+      # This value is NULL in MECAB_ONE_BEST mode.
       getter rpath : Wakame::Lib::MeCabPathT*
-      # pointer to the right path.
-      # this value is NULL if MECAB_ONE_BEST mode.
+      # Pointer to the right path.
+      # This value is NULL in MECAB_ONE_BEST mode.
       getter lpath : Wakame::Lib::MeCabPathT*
-      # surface string.
-      # this value is not 0 terminated.
-      # You can get the length with length/rlength members.
+      # Surface string
+      # This value is not 0 terminated.
+      # You can get the length from the length/rlength members.
       getter surface : LibC::Char*
-      # feature string
+      # Feature string
       getter feature : LibC::Char*
-      # unique node id
+      # Unique node id
       getter id : UInt32
-      # length of the surface form.
+      # Length of the surface form.
       getter length : UInt16
-      # length of the surface form including white space before the morph.
+      # Length of the surface form including the white space before the morph.
       getter rlength : UInt16
-      # right attribute id
+      # Right attribute id
       getter rc_attr : UInt16
-      # left attribute id
+      # Left attribute id
       getter lc_attr : UInt16
-      # unique part of speech id. This value is defined in "pos.def" file.
+      # Unique part of speech id.
+      # This value is defined in "pos.def" file.
       getter posid : UInt16
-      # character type
+      # Character type
       getter char_type : UInt8
-      # status of this model.
-      # This value is MECAB_NOR_NODE, MECAB_UNK_NODE, MECAB_BOS_NODE, MECAB_EOS_NODE, or MECAB_EON_NODE.
+      # Status of this model.
+      # This can be either MECAB_NOR_NODE, MECAB_UNK_NODE, MECAB_BOS_NODE, MECAB_EOS_NODE, or MECAB_EON_NODE.
       getter stat : UInt8
-      # set 1 if this node is best node.
+      # Set 1 if this node is the best node.
       getter isbest : UInt8
-      # forward accumulative log summation.
+      # Forward accumulative log summation.
       # This value is only available when MECAB_MARGINAL_PROB is passed.
       getter alpha : Float32
-      # backward accumulative log summation.
+      # Backward accumulative log summation.
       # This value is only available when MECAB_MARGINAL_PROB is passed.
       getter beta : Float32
-      # marginal probability.
+      # Marginal probability
       # This value is only available when MECAB_MARGINAL_PROB is passed.
       getter prob : Float32
-      # word cost.
+      # Word cost
       getter wcost : Int16
-      # best accumulative cost from bos node to this node.
+      # Best accumulative cost from bos node to this node.
       getter cost : LibC::Long
 
       # :nodoc:
